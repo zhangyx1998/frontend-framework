1c3b814e136c85eff7b6a75567bd5b409576c578
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-04-06 18:25:00 +0800

    Deployer

--- tools/deploy.mjs
+++
@@ -1,8 +0,0 @@
-import http from 'http'
-import { readFile, readFileSync, writeFileSync } from 'fs'
-import { createInterface } from 'readline'
-import { exec } from 'child_process'
-
-exec('pwd', (e, stdout, stderr) => {
-	console.log({ e, stdout, stderr })
-})
