cfff3132460a7e908fd4718ef5b7650176a75bb4
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-04-11 16:33:32 +0800

    update

---
+++ components/index.js
@@ -0,0 +1,24 @@
+// HTML Alternatives
+export { default as vBody } from './Body.vue'
+export { default as vForm } from './Form.vue'
+export { default as vFooter } from './Footer.vue'
+export { default as vHeader } from './Header.vue'
+// UI Building Blocks
+export { default as Responsive } from './Responsive.vue'
+export { default as StatusCode } from './StatusCode.vue'
+export { default as CodeEditor } from './CodeEditor.vue'
+export { default as Container } from './Container.vue'
+export { default as Redirect } from './Redirect.vue'
+export { default as Badge } from './Badge.vue'
+export { default as Btn } from './Button.vue'
+export { default as Tri } from './TriStateLink.vue'
+// User & Locale handlers (with possible http requests and other side effects)
+export { default as Avatar } from './Avatar.vue'
+export { default as LocaleName } from './locale/LocaleName.vue'
+// Animations
+export { default as AlternatingDots } from './spinners/AlternatingDots.vue'
+export { default as ChasingCircle } from './spinners/ChasingCircle.vue'
+export { default as ClassicSpinner } from './spinners/ClassicSpinner.vue'
+// External Components
+// import hljs from '@highlightjs/vue-plugin'
+// export const SyntaxHighlight = hljs.component
