47d51e4849c13371990176b5109bd09224e15d13
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-07-12 00:24:23 +0800

    MjxContainer: fix overflow and vertical align

--- components/MjxContainer.vue
+++ components/MjxContainer.vue
@@ -1,3 +1,12 @@
 <template>
-	<span><slot></slot></span>
+	<span mjx-container><slot></slot></span>
 </template>
+
+<style lang="scss" scoped>
+[mjx-container] {
+	display: inline-block;
+	overflow-x: scroll;
+	max-width: 100%;
+	vertical-align: middle;
+}
+</style>
